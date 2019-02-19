2027eb35450593b488c43a2d6923507c6b3eb925
yunik1004 <inkyuhak@gmail.com>
2019-02-20 00:37:55 +0900

    Remove tinyobjloader

--- src/lib/model.cpp
+++ src/lib/model.cpp
@@ -1,33 +1,12 @@
 #include "model.hpp"
-#ifndef TINYOBJLOADER_IMPLEMENTATION
-#define TINYOBJLOADER_IMPLEMENTATION
-#endif
-#include <tiny_obj_loader.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 #include "filesys.hpp"
 
 namespace YUNIK_GTC {
     ModelOBJ::ModelOBJ (void) {
-        tinyobj::attrib_t attrib;
-        std::vector<tinyobj::shape_t> shapes;
-        std::vector<tinyobj::material_t> materials;
-        std::string warn;
-        std::string err;
-        ifstream obj("model/box.obj");
-        
-        bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, &obj);
-        if (!err.empty()) {
-            std::cerr << err << std::endl;
-        }
-        if (!ret) {
-            std::cerr << "Error: cannot load model" << std::endl;
-        }
-        std::cout << "Load success" << std::endl;
-
-        printf("# of vertices  = %d\n", (int)(attrib.vertices.size()) / 3);
-        printf("# of normals   = %d\n", (int)(attrib.normals.size()) / 3);
-        printf("# of texcoords = %d\n", (int)(attrib.texcoords.size()) / 2);
-        printf("# of materials = %d\n", (int)materials.size());
-        printf("# of shapes    = %d\n", (int)shapes.size());
+        //
     }
 
     ModelOBJ::~ModelOBJ (void) {
